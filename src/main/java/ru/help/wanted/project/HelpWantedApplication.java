654fbb87f54b4f6e8ba58b6db621fb6294d645c2
ilakalasnikov <753476Kk>
2022-09-17 18:47:04 +0300

    Done: 1) Docker images created 2) Docker compose created

--- src/main/java/ru/help/wanted/project/HelpWantedApplication.java
+++ src/main/java/ru/help/wanted/project/HelpWantedApplication.java
@@ -1,33 +1,11 @@
 package ru.help.wanted.project;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import ru.help.wanted.project.model.entity.AppUser;
-import ru.help.wanted.project.model.entity.Role;
-import ru.help.wanted.project.repo.UserRepository;
-import ru.help.wanted.project.service.UserService;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class HelpWantedApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HelpWantedApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunnerBean(UserService UserService, UserRepository userRepository) {
-        return args -> {
-            UserService.saveRole(new Role(null, "ROLE_ADMIN"));
-            UserService.saveRole(new Role(null, "ROLE_USER"));
-
-            UserService.saveUser(new AppUser
-                    (null, "Ilya", "Kalashnikov", "753476Kk", "kalashilya@yandex.ru", true, new ArrayList<>()));
-            UserService.addRoleToUser("kalashilya@yandex.ru", "ROLE_ADMIN");
-        };
-    }
-
 }
